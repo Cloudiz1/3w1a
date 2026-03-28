cbc978c3a1829a8476a1c05ca54ae8849f1e4135
= <vincentni2007@gmail.com>
2026-03-28 14:17:08 -0400

    data types

---
+++ app/trails.d.ts
@@ -0,0 +1,33 @@
+export enum FilterKind { // under templates on whiteboard
+	Range,
+	Boolean, 
+	Categorical,
+	Misc,
+}
+
+export interface Trial {
+	name: String,
+	filters: Map<String, FilterKind>, // name to type
+	eligible: Array<Patient>,
+	consented: Array<Patient>,
+	trialData: Map<String, TrialData>, // id to data
+}
+
+export interface Patient {
+	name: String,
+	id: String,
+	attributes: Map<String, Attribute>, // name to data
+	notes: String,
+}
+
+export interface Attribute {
+	value: String,
+	type: FilterKind,
+}
+
+export interface TrialData {
+	independentVariable: Attribute,
+	before: Array<Attribute>,
+	after: Array<Attribute>,
+	notes: String,
+}
