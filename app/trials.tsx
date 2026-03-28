07bb61aad07ca81b5a0e156cd2b3aa1e9da2a66f
Evan Hoge <evanhoge123@gmail.com>
2026-03-28 17:16:33 -0400

    added a context menu!

--- app/trials.tsx
+++ app/trials.tsx
@@ -9,7 +9,15 @@ export function CreateTrialButton() {
     </>;
 }
 
-function TrialCreationMenu(setShowCreation) {
-    return <div className="">
+interface TrialCreationMenuProps {
+    setShowCreation: (value: boolean) => void;
+}
+function TrialCreationMenu({ setShowCreation }: TrialCreationMenuProps) {
+    return <div className="fixed inset-0 flex items-center justify-center">
+        <div className="bg-white w-1/6 h-1/6 rounded-xl flex flex-col">
+            <div className="flex justify-end">
+                <button onClick={() => setShowCreation(false)} className="text-red-500 pr-2">x</button>
+            </div>
+        </div>
     </div>;
 }
