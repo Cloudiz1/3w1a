c153c6f091911bdb073f500cd767b20606914548
unknown <mail.adambarnes@gmail.com>
2026-03-28 16:54:28 -0700

    homepage

---
+++ app/homepage.tsx
@@ -0,0 +1,8 @@
+import { CreateTrialButton } from "./trials";
+
+export default function homePage() {
+    return <div className="master">
+        <div className="editDiv"><CreateTrialButton/></div>
+        <div className="viewDiv"></div>
+        </div>
+}
