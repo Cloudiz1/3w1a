b03c1a49d0bdd05c0a90884de317a355065560c7
= <vincentni2007@gmail.com>
2026-03-28 17:37:51 -0400

    drag and drpo

--- app/filter.tsx
+++ app/filter.tsx
@@ -1,4 +1,5 @@
 import type { FilterKind } from "./types.js"
+"use client"
 
 export interface FilterProp {
 	name: String,
@@ -6,7 +7,20 @@ export interface FilterProp {
 }
 
 export function Filter({name, kind}: FilterProp) {
-	return <div className="top-0 left-0 bg-red-500 m-2 p-2 h-25">
+	return <div 
+		draggable
+		onDragStart={(e) => {
+			e.dataTransfer.setData("filter", JSON.stringify({ name, kind }))
+		}}
+		className="cursor-grab top-0 left-0 bg-red-500 m-2 p-2 h-25">
+
 		<p className="text-sm md:text-base lg:text-2xl text-center">{name}</p>
+	</div>
+}
+
+export function AppliedFilter({name, kind}: FilterProp) {
+	return <div className="h-1/4 w-1/5 shrink-0 m-2 bg-red-500">
+		<p>{name}</p>
+		<p>{kind}</p>
 	</div>
 }
