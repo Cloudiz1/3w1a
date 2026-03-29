664dda585ff547d8e25b7b70a15f25bc657e97f8
Emmett Goossen <egoossen1998@gmail.com>
2026-03-29 02:45:30 -0400

    fixed page

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import { CreateTrialButton } from "./trialButton";
 import { getTrials } from "./trialLogic";
 import { Trial } from "./types";
 import { TrialButton } from  "./trial";
-import { UpdatePeop } from "./updatePeop";
+import { PersonEditButton } from "./personModal";
 
 export default function homePage() {
     return <div className="master">
-        <div className="editDiv"><CreateTrialButton/><UpdatePeop/></div>
+        <div className="editDiv"><CreateTrialButton/><PersonEditButton/></div>
         <div className="viewDiv">
             {getTrials().map((trial: Trial, i: number) => {
                 return <TrialButton key={i} Trial={trial} />
